67ce9ad3b650af5db4207986865affa17e1f2c27
MisterAlexD <mralex.d@gmail.com>
2019-07-31 00:10:05 +0300

    Create IObservableCollection.cs

---
+++ AX.Common/IObservableCollection.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Text;
+
+namespace AX.Common
+{
+    public interface IObservableCollection<T> : ICollection<T>, INotifyCollectionChanged
+    {
+    }
+}
