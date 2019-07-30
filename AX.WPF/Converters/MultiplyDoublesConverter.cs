ac5c879747a5c9e9d1dd342795df0c8a4e11f89e
MisterAlexD <mralex.d@gmail.com>
2019-07-31 00:35:09 +0300

    Create MultiplyDoublesConverter.cs

---
+++ AX.WPF/Converters/MultiplyDoublesConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace AX.WPF.Converters
+{
+    public class MultiplyDoublesConverter : IMultiValueConverter
+    {
+        object IMultiValueConverter.Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            var result = 1d;
+            foreach (var value in values)
+            {
+                result *= (double)value;
+            }
+            return result;
+        }
+
+        object[] IMultiValueConverter.ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException("MultiplyDoublesConverter does not support ConvertBack");
+        }
+    }
+}
